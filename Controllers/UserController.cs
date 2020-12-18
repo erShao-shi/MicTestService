f8d630e76a44ed00dac88da58015731fbd2833b7
ershao.shi <1836340251@qq.com>
2020-12-18 17:56:19 +0800

    新增测试api

---
+++ Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicTestService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public string GetUserName()
+        {
+            return "ershao";
+        }
+    }
+}
